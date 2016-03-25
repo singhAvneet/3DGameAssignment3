35acc517652dd12958e4a9a7e314e18e41b1c214
Avneet <avneet_cochhar@hotmail.com>
2016-03-25 10:33:33 -0400

    added finish line and game over screen

---
+++ Assets/_Standard Assets/scoring/script/playerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerScript : MonoBehaviour {
+
+  
+  
+    private int count;
+	public GameController gamecontroller;
+    void start() {
+
+    
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+			gamecontroller.ScoreValue +=1;
+            
+
+        }
+
+		if (other.gameObject.CompareTag("finish")) {
+			other.gameObject.SetActive(false);
+			gamecontroller.LivesValue=-1;
+			gamecontroller.GameOverLabel.text = "You Win";
+
+
+		}
+    }
+
+
+   
+
+}
