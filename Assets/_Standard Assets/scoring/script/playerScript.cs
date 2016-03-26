d9bcc89503656ad6630c9084cb8bc373139ff983
Avneet <avneet_cochhar@hotmail.com>
2016-03-25 22:21:59 -0400

    final testing

--- Assets/_Standard Assets/scoring/script/playerScript.cs
+++ Assets/_Standard Assets/scoring/script/playerScript.cs
@@ -4,13 +4,15 @@ using UnityEngine.UI;
 
 public class playerScript : MonoBehaviour {
 
-  
+	[SerializeField]
+	private AudioSource _gameOverSound;
   
     private int count;
 	public GameController gamecontroller;
     void start() {
 
-    
+		this._gameOverSound = gameObject.GetComponent<AudioSource> ();
+			
     }
 
     
@@ -19,14 +21,14 @@ public class playerScript : MonoBehaviour {
        if (other.gameObject.CompareTag("Pick Up")) {
             other.gameObject.SetActive(false);
 			gamecontroller.ScoreValue +=1;
-            
+			this._gameOverSound.Play ();
 
         }
 
 		if (other.gameObject.CompareTag("finish")) {
 			other.gameObject.SetActive(false);
 			gamecontroller.LivesValue=-1;
-			gamecontroller.GameOverLabel.text = "You Win";
+			gamecontroller.GameOverLabel.text = "you Win";
 
 
 		}
